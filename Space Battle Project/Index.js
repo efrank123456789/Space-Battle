a2ca186de4d6f2eae3e2c808f321e02bf8a0ae89
efrank123456789 <haskel.frank@gmail.com>
2022-07-04 20:41:59 -0400

    Add files via upload

---
+++ Space Battle Project/Index.js
@@ -0,0 +1,65 @@
+class MyShip {
+    constructor(hull, firepower, accuracy){
+    this.hull = hull
+    this.firepower = firepower
+    this.accuracy = accuracy
+    }
+}
+
+
+
+
+class AlienShip1 {
+    constructor(hull, firepower, accuracy){
+    this.hull = hull
+    this.firepower = firepower
+    this.accuracy = accuracy
+    }
+
+    if (Math.random() < alien[0].accuracy) {
+        console.log('You have been hit!');
+    }
+    
+}
+
+
+
+class AlienShip2 {
+    constructor(hull, firepower, accuracy){
+    this.hull = hull
+    this.firepower = firepower
+    this.accuracy = accuracy
+    }
+}
+
+class AlienShip3 {
+    constructor(hull, firepower, accuracy){
+    this.hull = hull
+    this.firepower = firepower
+    this.accuracy = accuracy
+    }
+}
+
+class AlienShip4 {
+    constructor(hull, firepower, accuracy){
+    this.hull = hull
+    this.firepower = firepower
+    this.accuracy = accuracy
+    }
+}
+
+class AlienShip5 {
+    constructor(hull, firepower, accuracy){
+    this.hull = hull
+    this.firepower = firepower
+    this.accuracy = accuracy
+    }
+}
+
+class AlienShip6 {
+    constructor(hull, firepower, accuracy){
+    this.hull = hull
+    this.firepower = firepower
+    this.accuracy = accuracy
+    }
+}
